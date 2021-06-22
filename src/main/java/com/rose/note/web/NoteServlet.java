0229f5947a8ea0cf2d459107d5ee10ca829cdf25
ShayPatrickCormac <lhq8722861@gmail.com>
2021-06-22 10:59:50 +0800

    added add note page and able to get into that page

--- src/main/java/com/rose/note/web/NoteServlet.java
+++ src/main/java/com/rose/note/web/NoteServlet.java
@@ -1,11 +1,16 @@
 package com.rose.note.web;
 
+import com.rose.note.po.NoteType;
+import com.rose.note.po.User;
+import com.rose.note.service.NoteTypeService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/note")
 public class NoteServlet extends HttpServlet {
@@ -18,6 +23,11 @@ public class NoteServlet extends HttpServlet {
     }
 
     private void noteView(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = (User) req.getSession().getAttribute("user");
+        // get type list by user id
+        List<NoteType> typeList = new NoteTypeService().findTypeList(user.getUserId());
+        // set typelist to req field
+        req.setAttribute("typeList", typeList);
         req.setAttribute("changePage", "note/view.jsp");
         req.getRequestDispatcher("index.jsp").forward(req, resp);
 
